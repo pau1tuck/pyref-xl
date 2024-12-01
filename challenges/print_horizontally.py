7cb1ef8fa8116323f433ab5d5b627d57dff4ab9f
Paul Tuck <p6ul.tuck@gmail.com>
2024-12-01 12:33:19 +0700

    feat: Add print_horizontally challenge

---
+++ challenges/print_horizontally.py
@@ -0,0 +1,7 @@
+# Loop through the list [3, 6, 9, 12, 15] and print each element horizontally
+
+my_list = [3, 6, 9, 12, 15]
+for i in range(len(my_list)):
+    print(my_list[i], end=" ")
+
+# Output: 3 6 9 12 15
