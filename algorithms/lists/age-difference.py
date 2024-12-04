a600a39e6d28aa9c2377ddec3184bfe143f82d28
Paul Tuck <p6ul.tuck@gmail.com>
2024-12-04 09:33:33 +0700

    feat: Add age_difference array challenge

---
+++ algorithms/lists/age-difference.py
@@ -0,0 +1,16 @@
+# Given an array of objects, find the difference in age between the eldest and youngest family members, and return their respective ages and the age difference, e.g. [13, 67, 54]
+
+# Input: List of dictionaries, e.g. [{name: "Paul", age: 28}, {name: "John", age: 33}
+
+
+def find_age_difference(relatives):
+    ages = [relatives["age"] for relatives in relatives]
+    return [
+        min(ages),
+        max(ages),
+        max(ages) - min(ages),
+    ]  # [youngest, eldest, age difference]
+
+
+# Time Complexity: O(n), where n is the number of family members
+# Space Complexity: O(1)
