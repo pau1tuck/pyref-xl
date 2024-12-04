9bd5c5784f7cdfaf2501469190e852fba43828b8
Paul Tuck <p6ul.tuck@gmail.com>
2024-12-04 10:31:12 +0700

    refactor: Modify age-difference title

--- algorithms/lists/age_difference.py
+++ algorithms/lists/age_difference.py
@@ -1,9 +1,10 @@
 # Given an array of objects, find the difference in age between the eldest and youngest family members, and return their respective ages and the age difference, e.g. [13, 67, 54]
 
 # Input: List of dictionaries, e.g. [{name: "Paul", age: 28}, {name: "John", age: 33}
+from typing import List
 
 
-def find_age_difference(relatives):
+def find_age_difference(relatives: List[dict]) -> List[int]:
     ages = [relatives["age"] for relatives in relatives]
     return [
         min(ages),
